b60ac257c18efcfc4db9ce8ffbef99c2b3ed5ed5
Vinícius Velasco <vinicius.velasco@live.com>
2020-03-25 22:13:10 -0400

    Configuaração do microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      { name: 'HELLO_SERVICE', transport: Transport.TCP },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
